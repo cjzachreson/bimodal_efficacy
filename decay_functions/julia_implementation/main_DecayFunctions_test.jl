3695d582fb82d51429cfab8696779049e52b670c
Cameron Zachreson <cameron.zachreson@gmail.com>
2022-11-10 16:39:38 +1100

    module structure and plotting functions
    messed around with the module structure again and added some plotting functions to display histograms of efficacy and neuts.

--- decay_functions/julia_implementation/main_DecayFunctions_test.jl
+++ decay_functions/julia_implementation/main_DecayFunctions_test.jl
@@ -1,92 +1,30 @@
 #main_DecayFunctions_test.jl
 
-module Test_module
+using DataFrames, CSV, PlotlyJS
 
-include("DecayFunctions.jl")
-
-#__precompile__(false)
-
-#if !(pwd() in LOAD_PATH)
-#    push!(LOAD_PATH, pwd())
-#end
-
-#using .DecayFunctions
-using .DecayFunctions 
-
-
-using StatsBase
-using Distributions 
-using Random
-
-export main 
-function main(x_in::AbstractFloat, c50_in::AbstractFloat, k_in::AbstractFloat, delta_t::AbstractFloat)
-    # testing sigmoid mapping:
-    x = x_in
-    c50 = c50_in
-    k = k_in
-
-    println("c50: $c50")
-
-    y = sigmoid_1(c50, k, log(x))
-    println("sigmoid test complete:")
-    println("y = $y")
-
-    # testing double-exponential mapping: 
-    w1 = 0.5 #weight of decay component 1
-    w2 = 1.0 - w1 #weight of decay componenet 2 
-
-    v0 = 1.0
-
-    v0_1 = v0 * w1
-    v0_2 = v0 * w2
-    tau_1 = 1.0 / 0.0085
-    tau_2 = 1.0 / 0.00085
-
-    #delta_t = 100.0 
-
-    (y1, v1, v2) = double_exponential(v0_1, v0_2, tau_1, tau_2, delta_t)
-    println("double exponential test complete:")
-    println("y1 = $y1")
-
-
-
-    # generating a heterogeneous population: 
-    n = 10
-    neuts = zeros(n)
-    efficacy = zeros(n)
-
-    seed_neuts = 1
-    rng_neuts = MersenneTwister(seed_neuts)
-
-    mu = 1.0
-    sig = 0.5
-
-    neut_dist = LogNormal{Float64}(mu, sig)
-    rand!(rng_neuts, neut_dist, neuts)
-
-    println(neuts)
-
-    #TODO next: map each value in neuts through the sigmoidal function sigmoid_1()
-    iterator = 0
-    for neuts_i in neuts 
-        iterator += 1
-        #apply decay function: 
-        (neuts_dt, v1, v2) = double_exponential(neuts_i * w1, neuts_i * w2, tau_1, tau_2, delta_t)
-
-        efficacy[iterator] = sigmoid_1(c50, k, log(neuts_dt))
-    end
-
-    println(efficacy)
-
-end
-
-end 
+include("Test_module_main.jl")
 
 using .Test_module
 
-x_in = 1.0
+# neut dist. parameters (lognormal(mu, sig))
+mu = 1.0
+sig = 0.5
+
 c50_in = -0.5
 k_in = 5.0
-delta_t = 100.0
+delta_t = 300.0
+n = 100
 
-main(x_in, c50_in, k_in, delta_t)
+(neuts, eff) = generate_efficacy_distribution(mu, sig, c50_in, k_in, delta_t, n)
+
+df = DataFrame(log_titers = log.(neuts), Efficacy = eff)
+println("finished making dataframe")
+
+
+a = plot(df, x=:log_titers, kind = "histogram")
+b = plot(df, x=:Efficacy, kind = "histogram")
+
+display(a)
+display(b)
+
+println("finished plotting")
