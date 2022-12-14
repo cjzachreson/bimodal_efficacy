7f4025f97ad8cc9cc85cbf3e984b3d48bf044f76
Cameron Zachreson <cameron.zachreson@gmail.com>
2022-12-14 17:50:07 +1100

    added plots of neut dist before and after subsampling
    Neut dist gets sub-sampled through 'natural selection' skewed towards lower values in cohort of people infected. amount of skewing depends on k (and probably other params too).

--- correlated_outcomes_example/correlated_outcomes_example_MAIN_for_review.m
+++ correlated_outcomes_example/correlated_outcomes_example_MAIN_for_review.m
@@ -23,14 +23,15 @@ mu_neuts = mu_0 - tau * (t2 - t1);
 log_neut_dist = makedist('Normal', 'mu', mu_neuts, 'sigma', sd_neuts);
 
 % logistic transform parameters
-k = 2.4;
+%k = 2.4;
+k = 5 % just a test 
 L = 1;
 c50_acquisition = -1;
 c50_death = -2.7;
 
 % approximate efficacy dist with finite samples: 
 
-n = 100000;
+n = 100000; %number of individuals in both the control and trial groups
 log_neuts = random(log_neut_dist, [n, 1]);
 
 eff_acquisition = general_logistic(log_neuts, L, k, c50_acquisition);
@@ -48,35 +49,46 @@ eff_death_given_infection = 1 - rr_death_given_infection;
 CFR = 0.1; % base cfr p(death | infection, not vaccinated)
 
 % evaluate deaths as bernoulli trials from p(death | infection, vaccinated) 
-p_death_given_infection = CFR * (1 - eff_death_given_infection);
 
 % sample cases from cdf of rr_aq
-n_cases = 10000;
+n_cases_vac = 10000;
+n_cases_unvac = n_cases_vac * (1/mean(rr_aq));
 
-% finite distribution function
-cdf_rr_aq = cumsum(rr_aq) ./ sum(rr_aq);
+j = randsample(n, n_cases_vac, true, rr_aq.* 0.001);
+% is this sampling with or without replacement?? TODO: check this. 
 
-
-j = randsample(n, n_cases, true, rr_aq.* 0.001);
 cases_eff_death_given_infection = eff_death_given_infection(j);
 cases_eff_death = eff_death(j);
 cases_eff_acquisition = eff_acquisition(j);
 
+log_neuts_cases = log_neuts(j);
+
 
 % evaluate p_death_given_infection for sampled cases. 
-r = rand([n_cases, 1]);
+r = rand([n_cases_vac, 1]);
 
 p_death = CFR * (1 - cases_eff_death_given_infection);
 
-
 death = r < p_death; 
 
-CFR_vac = sum(death) / n_cases; 
+CFR_vac = sum(death) / n_cases_vac; 
 
-eff_obs = 1 - CFR_vac / CFR
+deathrate_vac = sum(death) / n; 
+deathrate_novac = (CFR * n_cases_unvac) / n; 
+
+eff_obs = 1 - (deathrate_vac / deathrate_novac) 
 
 eff_mean = mean(eff_death)
+eff_mean_cases = mean(cases_eff_death)
 
+figure(1)
+histogram(log_neuts, 'normalization', 'pdf')
+hold on
+histogram(log_neuts_cases, 'normalization', 'pdf')
+hold off
+
+figure(2)
+histogram(eff_acquisition)
 
 
 % figure(1)
